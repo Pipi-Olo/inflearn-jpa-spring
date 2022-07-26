f627ecdf9f6074aa26cd365df4d2002c6ea00a60
pipiolo <pipiolo1024@gmail.com>
2022-07-26 14:46:41 +0900

    Pageable 과 Web 의 관계 및 주의점

--- data-jpa/src/main/java/study/datajpa/dto/MemberDto.java
+++ data-jpa/src/main/java/study/datajpa/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.datajpa.dto;
 
 import lombok.Data;
+import study.datajpa.entity.Member;
 
 @Data
 public class MemberDto {
@@ -13,5 +14,11 @@ public class MemberDto {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    // DTO 는 엔티티에 의존해도 된다. 엔티티는 DTO 에 의존하면 안 된다.
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
     }
 }
